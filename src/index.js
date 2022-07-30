7365fe3056bc2f80d6157f7780ec8b44b936f222
navneetfw15 <navneet638864@gmail.com>
2022-07-30 10:00:42 +0530

    text

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+
+import {Cartcontextprovider} from "../src/cartcontext/Cartcontex"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Cartcontextprovider>
+    <BrowserRouter>
+    
     <App />
+    
+    </BrowserRouter>
+    </Cartcontextprovider>
+    
   </React.StrictMode>
 );
 
